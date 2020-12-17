8bdf4fd3d2a1e77bbcba3ede727c72161fc146ee
Cantepie <adrien.cantepie@6tm.com>
2020-12-18 00:29:58 +0100

    JS response

--- Resources/assets/Admin.js
+++ Resources/assets/Admin.js
@@ -15,13 +15,23 @@ import DateTimePicker from "umbrella_core/components/DateTimePicker";
 import FileUpload from "umbrella_core/components/FileUpload";
 import Collection from "umbrella_core/components/Collection";
 
+// js response action
+import ShowToast from "umbrella_core/jsresponse/action/ShowToast";
+import OpenModal from "umbrella_core/jsresponse/action/OpenModal";
+import CloseModal from "umbrella_core/jsresponse/action/CloseModal";
+import Eval from "umbrella_core/jsresponse/action/Eval";
+import Redirect from "umbrella_core/jsresponse/action/Redirect";
+import Reload from "umbrella_core/jsresponse/action/Reload";
+import RemoveHtml from "umbrella_core/jsresponse/action/RemoveHtml";
+import UpdateHtml from "umbrella_core/jsresponse/action/UpdateHtml";
+import ReloadTable from "umbrella_core/jsresponse/action/ReloadTable";
+
 // Default jquery plugin options
 $.fn.dataTable.ext.errMode = 'throw';
 $.toast.options.position = 'bottom-right';
 
 // App
 import UmbrellaApp from "umbrella_core/core/UmbrellaApp";
-import JsResponseHandler from "umbrella_core/core/AjaxJsResponseHandler";
 
 const app = new UmbrellaApp();
 window.app = app;
@@ -39,4 +49,12 @@ app.use('.js-datetimepicker', DateTimePicker);
 app.use('.js-umbrella-fileupload', FileUpload);
 app.use('.js-umbrella-collection', Collection);
 
-app.useAjaxHandler('default', new JsResponseHandler());
+app.jsResponseHandler.registerAction('toast', new ShowToast());
+app.jsResponseHandler.registerAction('open_modal', new OpenModal());
+app.jsResponseHandler.registerAction('close_modal', new CloseModal());
+app.jsResponseHandler.registerAction('eval', new Eval());
+app.jsResponseHandler.registerAction('redirect', new Redirect());
+app.jsResponseHandler.registerAction('reload', new Reload());
+app.jsResponseHandler.registerAction('update', new UpdateHtml());
+app.jsResponseHandler.registerAction('remove', new RemoveHtml());
+app.jsResponseHandler.registerAction('reload_table', new ReloadTable());
